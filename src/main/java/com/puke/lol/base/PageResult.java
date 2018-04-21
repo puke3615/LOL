ae24440481c5b28e22501cbdaf28cdbb97f20daf
puke3615 <1129090915@qq.com>
2018-04-21 22:21:51 +0800

    update PageResult

--- src/main/java/com/puke/lol/base/PageResult.java
+++ src/main/java/com/puke/lol/base/PageResult.java
@@ -16,29 +16,29 @@ public class PageResult<T> {
 
     private boolean success;
     private String msg;
-    private List<T> list;
+    private List<T> data;
     private int page = 1;
     private int pageSize = 10;
 
     public PageResult() {
     }
 
-    public PageResult(List<T> list, int page, int pageSize) {
-        this.list = list;
+    public PageResult(List<T> data, int page, int pageSize) {
+        this.data = data;
         this.page = page;
         this.pageSize = pageSize;
         this.success = true;
         this.msg = MSG_SUCCESS;
     }
 
-    public static <T> PageResult<T> success(PageQuery query, List<T> list) {
-        list = list == null ? Collections.emptyList() : list;
-        return new PageResult<>(list, query.getPage(), query.getPageSize());
+    public static <T> PageResult<T> success(PageQuery query, List<T> data) {
+        data = data == null ? Collections.emptyList() : data;
+        return new PageResult<>(data, query.getPage(), query.getPageSize());
     }
 
-    public static <T> PageResult<T> success(List<T> list) {
-        list = list == null ? Collections.emptyList() : list;
-        return new PageResult<>(list, 1, list.size());
+    public static <T> PageResult<T> success(List<T> data) {
+        data = data == null ? Collections.emptyList() : data;
+        return new PageResult<>(data, 1, data.size());
     }
 
     public static <T> PageResult<T> error() {
